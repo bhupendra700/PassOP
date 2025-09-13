3684ee0fb5af3090d185e230733347460d369c2b
bhupendra700 <bhupendra7021@gmail.com>
2025-09-13 18:13:02 +0530

    update frontend

--- frontend/src/config/axiosconfig.js
+++ frontend/src/config/axiosconfig.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 const userAxios = axios.create({
-    baseURL: "http://localhost:8000/user",
+    baseURL: "https://passop-sj3o.onrender.com/user",
     withCredentials: true
 })
 
 const authAxios = axios.create({
-    baseURL: "http://localhost:8000/auth",
+    baseURL: "https://passop-sj3o.onrender.com/auth",
 })
 
 const shareAxios = axios.create({
-    baseURL : "http://localhost:8000/share",
+    baseURL : "https://passop-sj3o.onrender.com/share",
     withCredentials : true
 })
 
