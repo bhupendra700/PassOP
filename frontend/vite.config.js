b29a0624f5738640728baa0a7bd3c27aa39b1298
bhupendra700 <bhupendra7021@gmail.com>
2025-09-13 20:10:31 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server :{
-    host:true,
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Passop App",          // Full name (install hone ke baad)
+        short_name: "Passop",        // Short name (home screen pe)
+        description: "Password Manager Application",
+        theme_color: "#ffffff",      // Top bar white
+        background_color: "#ffffff", // Splash screen bhi white
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
+  server: {
+    host: true,
   }
 })
