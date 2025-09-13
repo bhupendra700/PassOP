c247923ad758d2d139cbe92323ecc9a287d02d20
bhupendra700 <bhupendra7021@gmail.com>
2025-09-13 20:54:46 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,33 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      manifest: {
-        name: "PassOP",          // Full name (install hone ke baad)
-        short_name: "PassOP",     
-        description: "Password Manager Application",
-        theme_color: "#ffffff",      // Top bar white
-        background_color: "#ffffff", // Splash screen bhi white
-        display: "standalone",
-        start_url: "./",
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
-    })
   ],
   server: {
     host: true,
