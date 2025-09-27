59423322383644c7fc0f2e2506c9eb971dd405d5
bhupendra700 <bhupendra7021@gmail.com>
2025-09-27 16:46:26 +0530

    fixed nodemailer

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -6,6 +6,7 @@ import nodemailer from "nodemailer"
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS,
